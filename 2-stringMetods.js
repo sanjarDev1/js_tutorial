4bace41c13529831c741fe8eb3a46615bffa9ddd
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-25 13:27:21 +0500

    new lines

--- 2-stringMetods.js
+++ 2-stringMetods.js
@@ -7,4 +7,38 @@ console.log(str.length);
 // slice - matndan berilgan qiymat bo`ycha kesib oladi.
 
 var str = `JavaScript`
-console.log(str.slice(0,10));
+console.log(str.slice(0, 10));
+
+// substring - matndan berilgan qiymatlar bo`ycha kesib oladi
+
+var str = `Javascrit`
+console.log(str.substring(2,3));
+
+// substr - berilgan sondan keyin qancha belgi kesib olishni bildiradi
+
+var str = `Javascript`
+console.log(str.substr(0, 9));
+
+// padEnd - matndagi belgilar berilgan qiymat ga yetmaguncha 2-parametrda berilgan qiymatni ohirdan qo`shib ketadi
+
+var str = "hello"
+console.log(str.padEnd(10,'sa'));
+
+// padStart - matndagi belgilar berilgan qiymat ga yetmaguncha 2-parametrda berilgan qiymatni boshida qo`shib ketadi
+
+
+var str = "hello"
+console.log(str.padStart(10,2));
+
+// indexOf - berilgan qiymatni matnda nechinchi indexda ekanligini bildiradi 
+
+var str = "hello"
+console.log(str.indexOf("h"));
+
+// lastIndexOf - indexof bilan bir hil
+
+var str = "salom dunyo"
+console.log(str.lastIndexOf("u"));
+
+// 
+
