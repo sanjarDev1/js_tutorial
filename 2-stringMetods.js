3ca28eff1b08fac577f50cf6abc7305beceb54ef
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-25 12:56:59 +0500

    new file

---
+++ 2-stringMetods.js
@@ -0,0 +1,10 @@
+
+// length - matnda nechta belgi borligini chiqaradi
+var str = `Hello world`;
+console.log(str.length);
+
+
+// slice - matndan berilgan qiymat bo`ycha kesib oladi.
+
+var str = `JavaScript`
+console.log(str.slice(0,10));
