3448f34b1c7f403bd596deda2a0c6185bfddf10b
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-25 14:00:54 +0500

    task finished

--- 2-stringMetods.js
+++ 2-stringMetods.js
@@ -40,5 +40,38 @@ console.log(str.indexOf("h"));
 var str = "salom dunyo"
 console.log(str.lastIndexOf("u"));
 
-// 
+// search - berilgan so`zni nechchi indexdan boshlanishini qaytaradi
+
+var str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, possimus."
+console.log(str.search("Lor"));
+
+// includes - matnda berilgan qiymat bor yoki yoqligini qaytaradi true or false
+
+var str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, possimus."
+console.log(str.includes("t a"));
+
+// replace - 1-argumentni 2-argumentga ozgartiradi
+
+var str = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus, possimus."
+console.log(str.replace("Lor","sal"));
+
+// repeat -  matnni nech marta ko`paytrshga ishlatiladi 
+
+var str = "salom"
+console.log(str.repeat(6));
+
+// starsWith - matn berilgan qiymatdan boshlanadmi yoki boshlanmasligini bildiradi
+
+var str = "salom dumyo"
+console.log(str.startsWith('alo'));
+
+//  endsWith - matn berilgan qiymat ohirdan boshlanadmi yoki yoq boldiradi
+
+var str = "salom dumyo"
+console.log(str.endsWith('yo'));
+
+// trim - matndagi boshi va ohirdagi bosh joylarni o`chiradi
+
+var str = "          salom dumyo       "
+console.log(str.trim());
 
