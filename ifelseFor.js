342fc497f45155ca1adee9e199a2ca5c0855fa7b
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-10-29 10:31:47 +0500

    task1

---
+++ ifelseFor.js
@@ -0,0 +1,30 @@
+
+//  1-task
+
+var text = 'webBrain Academy';
+text = text.repeat(5);
+text = text.toLowerCase();
+text.replace(/\s/g, '');
+
+var counterA = 0;
+var counterB = 0;
+for (var i = 0; i < text.length; i++){
+    if (text[i] === ' ') {
+
+    }
+    // if (text[i] == 'a')
+    // {
+    //         counterA++;
+    // }
+    // if (text[i] == 'b') {
+    //     counterB++;
+    // }
+// console.log(text[i]);
+}
+console.log(counterA);
+console.log(counterB);
+console.log(text);
+
+
+// task 2
+
