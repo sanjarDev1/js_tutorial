e492794dd7133732b20fedc150a559239ce491c1
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-11-07 23:00:47 +0500

    task

---
+++ object.js
@@ -0,0 +1,24 @@
+users = [
+    {
+        id: 3,
+        year: 2010,
+        name: "Toshma",
+        status: "student",
+        key: "test",
+    },
+
+    {
+        id: 1,
+        year: 2011,
+        name: "Eshmat",
+        status: "student",
+        key: "test",
+    },
+    {
+        id: 2,
+        year: 2001,
+        name: "Ahmad",
+        status: "student",
+        key: "test",
+    },
+];
