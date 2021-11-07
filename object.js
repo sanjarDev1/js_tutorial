c14fcfe68b96728283c614e474478a527adeccb7
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-11-07 23:15:23 +0500

    task

--- object.js
+++ object.js
@@ -31,3 +31,37 @@ const sortedById = users.sort((x, y)=>{
 const sortedByYear = users.sort((x, y)=>{
     return x.year - y.year
 })
+
+// const sortedByName = users.sort((x, y)=>{
+//     if (x.name < y.name) return -1;
+//     if (x.name > y.name) return +1;
+//     return 0;
+// })
+
+// const sortedByName2 = users.sort((x, y)=>{
+//     return x.name.localeCompare(y.name);
+// })
+
+const onDelete1 = (arr, id) => {
+    let list = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id !== id) {
+            list.push(arr[i])
+        }
+    }
+    return list;
+}
+
+const onDelete2 = (arr, id) => {
+    let list = []
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i].id == id) {
+            list.push(arr[i])
+        }
+    }
+    return list;
+}
+
+
+
+console.log(onDelete1(users, 3))
