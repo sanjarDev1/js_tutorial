1538380e81ce469e1d3034e2c4d8cf745cbe8e7b
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-11-10 14:47:52 +0500

    task

--- object_task.js
+++ object_task.js
@@ -17,3 +17,22 @@ const checkSame = (value1, value2) => {
 
 console.log(checkSame(data, data2));
 console.log(checkSame(data2, data3));
+
+// 2. Object ichidagi Objectni qiymatlari mavjudligini tekshirish
+
+// const data = { a: { b: undefined } };
+// const data2 = { a: { b: 'it does exits' } };
+// const data3 = { a: { b: 'it does exits', innerKey: null } };
+
+// const nestedValueExists = (obj) => {
+//   if (obj.a.b === undefined) {
+//     return false
+//   }
+//   else if (obj.a.innerKey === null) {
+//     return false
+//   }else return true
+// }
+
+// console.log(nestedValueExists(data));
+// console.log(nestedValueExists(data2));
+// console.log(nestedValueExists(data3));
