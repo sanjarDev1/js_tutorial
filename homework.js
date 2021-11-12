cb8dadcb847f96f917820ba0275a41f1bf7162f3
sanjarbek <sanjarbekqobulov02@gmail.com>
2021-11-12 23:48:22 +0500

    tas

--- homework.js
+++ homework.js
@@ -1,17 +1,36 @@
-var ar = [1, 2, 8, 3, 5, 5, 8];
+// var ar = [1, 2, 8, 3, 5, 5, 8];
 
-var i = 0;
-var sorted = ar.sort();
-while (ar.length > i) {
-    console.log(ar);
-    var spliced = sorted.splice(sorted.length - 2, 2);
-    if (spliced[1] - spliced[0] !== 0) {
-        ar.push(spliced[1] - spliced[0]);
-        console.log(spliced);
-    }
-    sorted = ar.sort();
-    i++;
-}
+// var i = 0;
+// var sorted = ar.sort();
+// while (ar.length > i) {
+//     console.log(ar);
+//     var spliced = sorted.splice(sorted.length - 2, 2);
+//     if (spliced[1] - spliced[0] !== 0) {
+//         ar.push(spliced[1] - spliced[0]);
+//         console.log(spliced);
+//     }
+//     sorted = ar.sort();
+//     i++;
+// } 
 
-if (ar[0] - ar[1] === 0) ar = [];
-console.log(ar);
+// if (ar[0] - ar[1] === 0) ar = [];
+// console.log(ar);
+
+
+
+// console.log(0 );
+
+
+var ar = '0123456789';
+console.log(ar.lastIndexOf());
+
+
+console.log(eval('(3 ** 2 ** 2 )**(1/4)'))
+
+
+var ar = 'salom';
+console.log(ar.padStart(8));
+
+
+number = [5, 6, 78, 12, 3, 10, 4]
+console.log(number.fill(0,2,5));
